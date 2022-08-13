29aac8c279bf3bbd49c0dc267ceca298e3906330
Kaique José de Souza Santos <kaique30122004@gmail.com>
2022-08-13 11:15:17 -0300

    Alterações no EventSystem

--- Java/SkillCombat/Config/EventSystem.java
+++ Java/SkillCombat/Config/EventSystem.java
@@ -8,46 +8,13 @@ public class EventSystem {
     Scanner in = new Scanner(System.in);
     InstanceCharacter instance = new InstanceCharacter();
     private int rounds = 1;
-    private boolean isRound = rounds%2==0, isRepeat;
+    private boolean isRound = rounds % 2 == 0, isRepeat;
 
     public void gameStart(Player playerOne, Player playerTwo) {
         choseCharacter(playerOne);
         choseCharacter(playerTwo);
         battle(playerOne, playerTwo);
         battle(playerTwo, playerOne);
-    }
-
-    public void battle(Player attacker, Player defender) {
-        menuGame(attacker);
-        System.out.print("\nChoose your move");
-        int choice = in.nextInt();
-        isRepeat = true;
-        do {
-            switch (choice) {
-                case 1:
-                attacker.getCharacter().skillOne(defender);
-                    rounds += 1;
-                    break;
-                case 2:
-                attacker.getCharacter().skillTwo(defender);
-                    rounds += 1;
-                    break;
-                case 3:
-                attacker.getCharacter().skillThree(defender);
-                    rounds += 1;
-                    break;
-                case 4:
-                attacker.getCharacter().skillFour(defender);
-                    rounds += 1;
-                    break;
-                case 5:
-                    rounds += 1;
-                    break;
-                default:
-                    System.out.println("Invalid value");
-                    break;
-            }
-        } while (isRepeat);
     }
 
     public void choseCharacter(Player player) {
@@ -69,6 +36,64 @@ public class EventSystem {
                 break;
         }
     }
+
+    public void battle(Player attacker, Player defender) {
+        menuGame(attacker);
+        isRepeat = true;
+        do {
+            System.out.print("\nChoose your move");
+            int choice = in.nextInt();
+            switch (choice) {
+                case 1:
+                    attacker.getCharacter().skillOne(defender);
+                    checkLife(attacker, defender);
+                    rounds += 1;
+                    isRepeat = false;
+                    break;
+                case 2:
+                    attacker.getCharacter().skillTwo(defender);
+                    checkLife(attacker, defender);
+                    rounds += 1;
+                    isRepeat = false;
+                    break;
+                case 3:
+                    attacker.getCharacter().skillThree(defender);
+                    checkLife(attacker, defender);
+                    rounds += 1;
+                    isRepeat = false;
+                    break;
+                case 4:
+                    attacker.getCharacter().skillFour(defender);
+                    checkLife(attacker, defender);
+                    rounds += 1;
+                    isRepeat = false;
+                    break;
+                case 5:
+                    rounds += 1;
+                    isRepeat = false;
+                    break;
+                default:
+                    System.out.println("\nInvalid value");
+                    isRepeat = true;
+                    break;
+            }
+        } while (isRepeat);
+    }
+
+    public void checkLife(Player attacker, Player defender) {
+        if (attacker.getCharacter().getLife() <= 0) {
+            defender.setScore(defender.getScore() + 1);
+            gameOver(defender);
+        } else if (defender.getCharacter().getLife() <= 0) {
+            attacker.setScore(attacker.getScore() + 1);
+            gameOver(attacker);
+        }
+    }
+
+    public void gameOver(Player winner){
+        
+    }
+    
     public void menuGame(Player player) {
         System.out.println("\n----------SKILL COMBAT----------");
         System.out.print("\n"+player.getName()+" turn");
@@ -83,5 +108,9 @@ public class EventSystem {
         System.out.print("3 - ["+player.getCharacter().getSkills()[2]+"] "+"["+player.getCharacter().getStaminaCost()[2]+"]  ");
         System.out.print("4 - ["+player.getCharacter().getSkills()[3]+"] "+"["+player.getCharacter().getStaminaCost()[3]+"]\n");
         System.out.print("5 - Skip the round\n");
+    }
+
+    public boolean isRound() {
+        return isRound;
     }
 }
